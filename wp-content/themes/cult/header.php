81935f2be08fea2c0f0182bd8c8a0b76e705b1e4
roman96 <isyk.roman@gmail.com>
2023-11-28 23:59:20 +0200

    seo3

--- wp-content/themes/cult/header.php
+++ wp-content/themes/cult/header.php
@@ -5,7 +5,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(''); ?></title>
 
